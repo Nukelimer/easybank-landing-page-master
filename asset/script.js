b32c82841cce9c694567b080b120fe28edab564d
codewithremi <codewithremi@gmail.com>
2023-02-16 03:09:53 +0100

    tailwind...

--- asset/script.js
+++ asset/script.js
@@ -1,17 +1,11 @@
-const hamburger = document.querySelector('.hamburger')
-const cancel = document.querySelector('.cancel')
-const hid = document.querySelector('.hid')
-
-
-hamburger.addEventListener('click', () => {
-   
-        hid.style.display = 'flex';
-
-
-})
+const hamburger = document.querySelector('.hamburger');
+const cancel = document.querySelector('.cancel');
+const hid = document.querySelector('.hid');
 
 cancel.addEventListener('click', () => {
-    
-        hid.style.display = 'none';
+  hid.style.display = 'none';
+});
+hamburger.addEventListener('click', () => {
+  hid.style.display = 'flex';
+});
 
-})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}', '../index.html'],
+  content: ['./src/**/*.{html,js}','./index.html'],
   theme: {
-  //   screens: {
-  //     'small': { 'max': '639px' },
-  //     'medium': { 'max': '767px'}
-  // },
     fontFamily: {
       sans: [
         'Montserrat, sans-serif',
